f322bfb8b619cad87c40f6c14db305d7a4982019
ssaykos <ssaykos@ssaykos-PC>
2018-05-18 13:26:09 +0900

    UTF-8변경

--- JAVA OP Tutoial/src/OP_Tutorial/console.java
+++ JAVA OP Tutoial/src/OP_Tutorial/console.java
@@ -2,7 +2,7 @@ package OP_Tutorial;
 
 public class console {
 	public static void main(String[] args) {
-		System.out.println("Çï·Î¿ì¿ùµå");
-		System.out.println("helloworld");
+		System.out.println("í—¬ë¡œìš°ì›”ë“œ");
+		System.out.println("hello world");
 	}
 }
