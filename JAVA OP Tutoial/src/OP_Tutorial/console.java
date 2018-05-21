0277256323f1ef17d641fac883c11ab6e2d7818e
ssaykos <ssaykos@ssaykos-PC>
2018-05-21 15:07:56 +0900

    UTF-8설명전

--- JAVA OP Tutoial/src/OP_Tutorial/console.java
+++ JAVA OP Tutoial/src/OP_Tutorial/console.java
@@ -2,7 +2,7 @@ package OP_Tutorial;
 
 public class console {
 	public static void main(String[] args) {
-		System.out.println("í—¬ë¡œì›”ë“œ");
+		System.out.println("Çï·Î¿ùµå");
 		System.out.println(123);
 		System.out.println("helloworld");
 		System.out.println('a');
