2a871e97065d3fa1a3c1f2b031bdea3c761c22e0
ssaykos <ssaykos@ssaykos-PC>
2018-05-21 15:18:03 +0900

    문자인코딩UTF-8로변경

--- JAVA OP Tutoial/src/OP_Tutorial/console.java
+++ JAVA OP Tutoial/src/OP_Tutorial/console.java
@@ -2,7 +2,7 @@ package OP_Tutorial;
 
 public class console {
 	public static void main(String[] args) {
-		System.out.println("헬로우월드 문자인코딩 MS949에서 UTF8로 바꿈");
+		System.out.println("헬로우월드 문자인코딩 MS949에서 UTF8로 변경");
 		System.out.println(123);
 		System.out.println("helloworld");
 		System.out.println('a');
