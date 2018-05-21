9a9cb6684902ab22b961d22ed6debe8144fede34
ssaykos <ssaykos@ssaykos-PC>
2018-05-21 14:37:06 +0900

    수정

--- JAVA OP Tutoial/src/OP_Tutorial/console.java
+++ JAVA OP Tutoial/src/OP_Tutorial/console.java
@@ -1,8 +1,12 @@
 package OP_Tutorial;
 
 public class console {
-	public static void main(String[] args) {
-		System.out.println("헬로우월드");
-		System.out.println("hello world");
-	}
+public static void main(String[] args) {
+	System.out.println("헬로월드");
+	System.out.println(123);
+	System.out.println("helloworld");
+	System.out.println('a');
+	System.out.println(true);
+	
+}
 }
